26b74e978ff4f26c2a14fcd9a20d3f388df11c48
Thales Lima <thales.bplima@gmail.com>
2022-02-04 18:36:01 -0300

    practice express 22.4

---
+++ hello-express/index.js
@@ -0,0 +1,14 @@
+// Estrutura Básica para aplicação Express
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', handleHelloWorldRequest);
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
+
+function handleHelloWorldRequest(req, res) {
+  res.status(200).send('Hello World!');
+}
