c1577ebb0c20739829b5b3002c50ab66d1ea1a5e
thales-bpl <thales.bplima@gmail.com>
2022-04-29 19:46:08 -0300

    impl practice exercise. impl getAll endpoint.

---
+++ practice/express-typescript/DUVIDAS.md
@@ -0,0 +1,5 @@
+1. ./models/book.model.ts - L20:
+  Qual é a função do <ResultSetHeader> nessa linha? Porque não precisa dele na L13?
+
+2. ./controllers/book.controller.ts:
+  Porque o controller não precisa bindar o BookService (usando this._BookService = BookService no corpo do contructor)?
---
+++ practice/express-typescript/models/book.model.ts
@@ -0,0 +1,25 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import Book from '../interfaces/book.interface';
+
+export default class BookModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection
+  }
+
+  public async getAll(): Promise<Book[]> {
+    const query = 'SELECT * FROM books';
+    const [rows] = await this.connection.execute(query)
+    return rows as Book[];
+  }
+
+  public async create(book: Book): Promise<Book> {
+    const { title, price, author, isbn } = book;
+    const query = 'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)';
+    const result = await this.connection.execute<ResultSetHeader>(query, [title, price, author, isbn]);
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...book };
+  }
+}
